bfc011b0aa2e37f69de1e20ab58e127775571906
Toby Clemson <tobyclemson@gmail.com>
2019-09-12 15:22:49 +0100

    Version 0.0.13-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/liberator-hal-events-resource "0.0.12"
+(defproject b-social/liberator-hal-events-resource "0.0.13-SNAPSHOT"
   :description "A HAL events resource for liberator."
   :url "https://github.com/b-social/liberator-hal-events-resource"
 
